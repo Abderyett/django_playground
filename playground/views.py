fa18b30aba1484e7228cf44fefe5cb8424cb3c25
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-11-19 10:29:24 +0100

    install django_debug_toolbar

--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def say_hello(response):
-   return HttpResponse("hello world")
+def say_hello(request):
+   return render(request, 'hello.html', {'name':'Abderyett'})
+
+
+def products(request):
+   return render(request, 'products.html',{"name":"Chicken brest"})
+
--- storefront/urls.py
+++ storefront/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+   
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
+    
 ]
