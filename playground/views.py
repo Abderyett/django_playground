1c73fe89fe6e1e812860ee0ca3c2b8cead1e85d4
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-12-02 20:23:13 +0100

    working with filtering

--- playground/views.py
+++ playground/views.py
@@ -4,6 +4,16 @@ from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
+    # product=Product.objects.get(pk=1)
+    # query_set=Product.objects.all()
+    
+    # exist = Product.objects.filter(pk=1).exists()
+    
+    # queryset = Product.objects.filter(unit_price_gt=20)
+    # queryset = Product.objects.filter(unit_price__range=(20,30))
+    queryset = Product.objects.filter(title__icontains='coffee')
+    # queryset = Product.objects.filter(discription__isnull=True)
+    
+    
 
-    return render(request, "hello.html", {"name": "AbderYett"})
+    return render(request, "hello.html", {"name": "AbderYett","products":list(queryset)})
